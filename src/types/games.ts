9c46d30c6edd167b41658eb63d5ee879a56db612
CaptnJayce <captn.dev@protonmail.com>
2026-05-28 00:37:56 +0100

    chore: Add WIP games

--- src/types/games.ts
+++ src/types/games.ts
@@ -1,14 +1,20 @@
 export const myGames = [
-    {
-        title: "Perpetua",
-        url: "/",
-        image: "/palceholder.png",
-        author: "Me",
-    },
-    {
-        title: "The Switch",
-        url: "https://switch-board.vercel.app/",
-        image: "/theswitch.png",
-        author: "Ibrahim",
-    },
+  {
+    title: "Perpetua",
+    url: "https://perpetua-game.netlify.app/",
+    image: "/perpetua.png",
+    author: "Me",
+  },
+  {
+    title: "Tower of Tomes",
+    url: "https://tower-of-tomes.netlify.app/",
+    image: "/towerofthetomes.png",
+    author: "Me",
+  },
+  {
+    title: "The Switch",
+    url: "https://switch-board.vercel.app/",
+    image: "/theswitch.png",
+    author: "Ibrahim",
+  },
 ];
